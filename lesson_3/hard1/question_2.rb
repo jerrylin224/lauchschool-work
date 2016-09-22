b173282bb93e41ff25bc2d936bf8c1a56c91f5a8
Mata Angelou <pk60905@hotmail.com>
2016-09-22 15:12:36 +0800

    Modified

---
+++ lesson_3/hard1/question_2.rb
@@ -0,0 +1,4 @@
+The output is {:a=>"hi there"}
+#informal_greeting is a reference to the original object. 
+#So when we mutate informal_greeting actually we are mutating the number it reference,original object.
+#Therefore when we call the "original object",it has become the "new object"
