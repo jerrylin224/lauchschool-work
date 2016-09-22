1e1035341532e20ee74aa29bad68a7209176c2ed
Mata Angelou <pk60905@hotmail.com>
2016-09-22 14:53:53 +0800

    Modified

--- lesson_3/easy2/question_2.rb
+++ lesson_3/easy2/question_2.rb
@@ -1,6 +1,6 @@
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 
-so I need to do something to print out value and sum all of them.
+ages.values.inject(:+)
 
 
 #Use underscore for unused parameters
